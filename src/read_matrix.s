195a0662033c466b13ab8ec0600e977b34a71ddc
exuanbo <exuanbo@protonmail.com>
2022-05-29 16:12:33 +0100

    done with Part B Task 1: Read Matrix

--- src/read_matrix.s
+++ src/read_matrix.s
@@ -16,7 +16,7 @@
 # Returns:
 #   a0 (int*)  is the pointer to the matrix in memory
 #
-# If you receive an fopen error or eof, 
+# If you receive an fopen error or eof,
 # this function exits with error code 50.
 # If you receive an fread error or eof,
 # this function exits with error code 51.
@@ -26,16 +26,91 @@
 read_matrix:
 
     # Prologue
-	
+    addi sp, sp, -20
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    sw s2, 12(sp)
+    sw s3, 16(sp)
 
+    mv s0, a0       # the address of the filename
+    mv s1, a1       # the address of the number of rows
+    mv s2, a2       # the address of the number of columns
 
+    # open file
+    mv a1, s0       # fopen a1
+    li a2, 0        # read only permission
+    jal fopen
+    bge zero, a0, fopen_error
+    mv s0, a0       # the file descriptor
 
+    # allocate memory for the buffer
+    li a0, 8        # 8 bytes for 2 4-byte numbers
+    jal malloc
+    beqz a0, malloc_error
+    mv s3, a0       # the address of the buffer
 
+    # read the dimensions into the buffer
+    mv a1, s0       # fread a1
+    mv a2, s3       # fread a2
+    li a3, 8        # fread a3
+    jal fread
+    li t0, 8
+    bne a0, t0, fread_error
+    lw t1, 0(s3)    # the number of rows
+    lw t2, 4(s3)    # the number of columns
+    sw t1, 0(s1)    # store the number of rows to memory
+    sw t2, 0(s2)    # store the number of columns to memory
+    # s1 and s2 now can be reused
+    mul t0, t1, t2
+    slli s1, t0, 2  # the length of the array
 
+    # free the memory of the buffer
+    mv a0, s3       # free a0
+    jal free
 
+    # allocate memory for the matrix
+    mv a0, s1       # malloc a0
+    jal malloc
+    beqz a0, malloc_error
+    mv s2, a0       # the address of the matrix
 
+    # read the rest of the file into the matrix
+    mv a1, s0       # fread a1
+    mv a2, s2       # fread a2
+    mv a3, s1       # fread a3
+    jal fread
+    bne a0, s1, fread_error
+
+    # close file
+    mv a1, s0       # fclose a1
+    jal fclose
+    bnez a0, fclose_error
+
+    # return value
+    mv a0, s2
 
     # Epilogue
-
-
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    addi sp, sp, 20
     ret
+
+malloc_error:
+    li a1, 48
+    jal exit2
+
+fopen_error:
+    li a1, 50
+    jal exit2
+
+fread_error:
+    li a1, 51
+    jal exit2
+
+fclose_error:
+    li a1, 52
+    jal exit2
--- tests/assembly/test_read_matrix.s
+++ tests/assembly/test_read_matrix.s
@@ -8,10 +8,31 @@ file_path: .asciiz "inputs/test_read_matrix/test_input.bin"
 main:
     # Read matrix into memory
 
+    # load the address of the filename
+    la s0, file_path
 
+    # allocate memory for the number of rows
+    li a0, 4
+    jal malloc
+    mv s1, a0
+
+    # allocate memory for the number of columns
+    li a0, 4
+    jal malloc
+    mv s2, a0
+
+    # call read_matrix
+    mv a0, s0
+    mv a1, s1
+    mv a2, s2
+    jal read_matrix
+    mv s3, a0
 
     # Print out elements of matrix
-
-
+    mv a0, s3
+    lw a1, 0(s1)
+    lw a2, 0(s2)
+    jal print_int_array
 
     # Terminate the program
+    jal exit
